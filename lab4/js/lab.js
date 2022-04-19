3bcb75e5d82fd77f324c3fd90ad848d895bbaba0
Phil Meyer <phmeyer@ucsc.edu>
2022-04-19 05:48:29 -0700

    lab4 publish

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+// Lab 4 Pseudocode.
+// Practice thinking like programmers.
+// @link   https://meyerphil.github.io/art101/lab4/index.html
+// @file   This file holds pseudocode for the lab.
+// @author Phil Meyer.
+// @since  4.19.22
+
+// Creating a to-do list:
+
+    // Print out the list
+    // Accept user input
+    // if input is add item 
+        // add item to list
+    // if input is cross item
+        // cross out item on list
+    // if input is remove item
+        // remove item from list
+    // Update list and go back to step 1
