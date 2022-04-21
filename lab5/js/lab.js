4fe97a0609090aeea59fdc47b31df9dd44692b00
Phil Meyer <phmeyer@ucsc.edu>
2022-04-21 03:13:00 -0700

    upload lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+// Lab 5 Datatypes and Variables.
+// Practice using variables in JS.
+// @link   https://meyerphil.github.io/art101/lab5/index.html
+// @file   This file holds code for lab
+// @author Phil Meyer.
+// @since  4.21.22
+
+// declare and init. variables
+var make = "Dodge";
+var model = "Challenger";
+var color = "Black";
+var year = 2019;
+var ownIt = false;
+var age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("OwnIt: " + ownIt + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
