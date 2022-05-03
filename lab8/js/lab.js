3af12674fad0cc17ddfebc81c76791e30c09ee0b
Phil Meyer <phmeyer@ucsc.edu>
2022-05-03 04:00:40 -0700

    lab8

---
+++ lab8/js/lab.js
@@ -0,0 +1,23 @@
+// Lab 8 Anon Function
+// Practice using Anon Functions and Callback
+// @link   https://meyerphil.github.io/art101/lab8/index.html
+// @file   This file holds code for lab
+// @author Phil Meyer.
+// @since  5.3.22
+
+// declare
+var numbers = [12,4,3,5,9,7,15]
+
+// checks if inputted number is a multiple of 3
+function isMultOfThree(x){
+    var results = x % 3;
+    return (results == 0);
+}
+
+// main 
+console.log("MultOfThree(27): " + isMultOfThree(27));
+console.log("MultOfThree(8): " + isMultOfThree(8));
+console.log("check array: " + numbers + ", are they mult of 3? " + numbers.map(isMultOfThree));
+console.log("multiply array by 100: " + numbers.map(function(x){
+    return x * 100;
+}));
